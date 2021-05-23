778711cd3ef28974bb66308a5f693ee814351d6a
tapioki <xtra.onyx@gmail.com>
2021-05-23 23:13:17 +0200

    Added new switch footprints, updated reversibility functionality, integrated hotswap footprints, added orientation to promicro, alphabetized index, and added net and paramter documentation

--- src/footprints/mx.js
+++ src/footprints/mx.js
@@ -1,12 +1,25 @@
+// Any MX switch
+// Nets
+//    from: corresponds to pin 1
+//    to: corresponds to pin 2
+// Params
+//    hotswap: default is false
+//      if true, will include holes and pads for Kailh MX hotswap sockets
+//    reverse: default is false
+//      if true, will flip the footprint such that the pcb can be reversible 
+//
+// note: hotswap and reverse can be used simultaneously
+
 module.exports = {
   nets: ['from', 'to'],
   params: {
-    class: 'S'
+      class: 'S',
+      hotswap: false,
+      reverse: false
   },
-  body: p => `
-
-    (module MX (layer F.Cu) (tedit 5DD4F656)
-
+  body: p => {
+    const standard = `
+      (module MX (layer F.Cu) (tedit 5DD4F656)
       ${p.at /* parametric position */}
 
       ${'' /* footprint reference */}
@@ -22,18 +35,45 @@ module.exports = {
       (fp_line (start 7 -7) (end 6 -7) (layer Dwgs.User) (width 0.15))
       (fp_line (start 6 7) (end 7 7) (layer Dwgs.User) (width 0.15))
       (fp_line (start 7 -7) (end 7 -6) (layer Dwgs.User) (width 0.15))
-      
-      ${''/* pins */}
-      (pad 1 thru_hole circle (at 2.54 -5.08) (size 2.286 2.286) (drill 1.4986) (layers *.Cu *.Mask) ${p.net.from})
-      (pad 2 thru_hole circle (at -3.81 -2.54) (size 2.286 2.286) (drill 1.4986) (layers *.Cu *.Mask) ${p.net.to})
-
+    
       ${''/* middle shaft */}
       (pad "" np_thru_hole circle (at 0 0) (size 3.9878 3.9878) (drill 3.9878) (layers *.Cu *.Mask))
 
       ${''/* stabilizers */}
       (pad "" np_thru_hole circle (at 5.08 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
       (pad "" np_thru_hole circle (at -5.08 0) (size 1.7018 1.7018) (drill 1.7018) (layers *.Cu *.Mask))
-    )
-
-  `
+      )
+      `
+    function pins(def_neg, def_pos, def_side) {
+      if(p.param.hotswap == TRUE) {
+        return `
+        ${'' /* holes */}
+        (pad "" np_thru_hole circle (at ${def_pos}2.54 -5.08) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at ${def_neg}3.81 -2.54) (size 3 3) (drill 3) (layers *.Cu *.Mask))
+        
+        ${'' /* net pads */}
+        (pad 1 smd rect (at ${def_neg}7.085 -2.54 180) (size 2.55 2.5) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.from})
+        (pad 2 smd rect (at ${def_pos}5.842 -5.08 180) (size 2.55 2.5) (layers ${def_side}.Cu ${def_side}.Paste ${def_side}.Mask) ${p.net.to})
+        `
+      } else {
+          return `
+            ${''/* pins */}
+            (pad 1 thru_hole circle (at ${def_pos}2.54 -5.08) (size 2.286 2.286) (drill 1.4986) (layers *.Cu *.Mask) ${p.net.from})
+            (pad 2 thru_hole circle (at ${def_neg}3.81 -2.54) (size 2.286 2.286) (drill 1.4986) (layers *.Cu *.Mask) ${p.net.to})
+          `
+      }
+    }
+    if(p.param.reverse){
+      return `
+        ${standard}
+        ${pins('-', '', 'B')}
+        ${pins('', '-', 'F')})
+        `
+    } else {
+      return `
+        ${standard}
+        ${pins('-', '', 'B')})
+        `
+    }
+  }
 }
---
+++ src/footprints/scrollwheel.js
@@ -0,0 +1,79 @@
+// Panasonic EVQWGD001 horizontal rotary encoder
+//
+//   __________________
+//  (f) (t)         | |
+//  | (1)           | |
+//  | (2)           | |
+//  | (3)           | |
+//  | (4)           | |
+//  |_( )___________|_|
+//
+// Nets
+//    from: corresponds to switch pin 1 (for button presses)
+//    to: corresponds to switch pin 2 (for button presses)
+//    A: corresponds to pin 1 (for rotary)
+//    B: corresponds to pin 2 (for rotary, should be GND)
+//    C: corresponds to pin 3 (for rotary)
+//    D: corresponds to pin 4 (for rotary, unused)
+// Params
+//    reverse: default is false
+//      if true, will flip the footprint such that the pcb can be reversible
+
+
+module.exports = {
+    nets: ['from', 'to', 'A', 'B', 'C', 'D'],
+    params: {
+      class: 'S',
+		  reverse: false
+    },
+    body: p => {
+      standard = `
+        (module RollerEncoder_Panasonic_EVQWGD001 (layer F.Cu) (tedit 6040A10C)
+        ${p.at /* parametric position */}   
+        (fp_text reference REF** (at 0 0 ${p.rot}) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
+        (fp_text value RollerEncoder_Panasonic_EVQWGD001 (at -0.1 9 ${p.rot}) (layer F.Fab) (effects (font (size 1 1) (thickness 0.15))))
+        
+        ${'' /* corner marks */}
+        (fp_line (start -8.4 -6.4) (end 8.4 -6.4) (layer Dwgs.User) (width 0.12))
+        (fp_line (start 8.4 -6.4) (end 8.4 7.4) (layer Dwgs.User) (width 0.12))
+        (fp_line (start 8.4 7.4) (end -8.4 7.4) (layer Dwgs.User) (width 0.12))
+        (fp_line (start -8.4 7.4) (end -8.4 -6.4) (layer Dwgs.User) (width 0.12))
+      `
+      function pins(def_neg, def_pos) {
+        return `
+          ${'' /* edge cuts */}
+          (fp_line (start ${def_pos}9.8 7.3) (end ${def_pos}9.8 -6.3) (layer Edge.Cuts) (width 0.15))
+          (fp_line (start ${def_pos}7.4 -6.3) (end ${def_pos}7.4 7.3) (layer Edge.Cuts) (width 0.15))
+          (fp_line (start ${def_pos}9.5 -6.6) (end ${def_pos}7.7 -6.6) (layer Edge.Cuts) (width 0.15))
+          (fp_line (start ${def_pos}7.7 7.6) (end ${def_pos}9.5 7.6) (layer Edge.Cuts) (width 0.15))
+          (fp_arc (start ${def_pos}7.7 7.3) (end ${def_pos}7.4 7.3) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
+          (fp_arc (start ${def_pos}9.5 7.3) (end ${def_pos}9.5 7.6) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
+          (fp_arc (start ${def_pos}7.7 -6.3) (end ${def_pos}7.7 -6.6) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
+          (fp_arc (start ${def_pos}9.5 -6.3) (end ${def_pos}9.8 -6.3) (angle ${def_neg}90) (layer Edge.Cuts) (width 0.15))
+
+          ${'' /* pins */}
+          (pad S1 thru_hole circle (at ${def_neg}6.85 -6.2 ${p.rot}) (size 1.6 1.6) (drill 0.9) (layers *.Cu *.Mask) ${p.net.from})
+          (pad S2 thru_hole circle (at ${def_neg}5 -6.2 ${p.rot}) (size 1.6 1.6) (drill 0.9) (layers *.Cu *.Mask) ${p.net.to})
+          (pad A thru_hole circle (at ${def_neg}5.625 -3.81 ${p.rot}) (size 1.6 1.6) (drill 0.9) (layers *.Cu *.Mask) ${p.net.A})
+          (pad B thru_hole circle (at ${def_neg}5.625 -1.27 ${p.rot}) (size 1.6 1.6) (drill 0.9) (layers *.Cu *.Mask) ${p.net.B})
+          (pad C thru_hole circle (at ${def_neg}5.625 1.27 ${p.rot}) (size 1.6 1.6) (drill 0.9) (layers *.Cu *.Mask) ${p.net.C})
+          (pad D thru_hole circle (at ${def_neg}5.625 3.81 ${p.rot}) (size 1.6 1.6) (drill 0.9) (layers *.Cu *.Mask) ${p.net.D})
+
+          ${'' /* stabilizer */}
+          (pad "" np_thru_hole circle (at ${def_neg}5.625 6.3 ${p.rot}) (size 1.5 1.5) (drill 1.5) (layers *.Cu *.Mask))
+        `
+    }
+    if(p.param.reverse) {
+      return `
+        ${standard}
+        ${pins('-', '')}
+        ${pins('', '-')})
+        `
+    } else {
+      return `
+        ${standard}
+        ${pins('-', '')})
+        `
+    }
+  }
+}
