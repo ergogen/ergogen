7550a5da447e2ed92387f64f9488c712acfd1ec5
Miigon <miigon.pg@gmail.com>
2022-05-03 07:14:46 +0800

    TRRS: add missing stabilizer hole for reverse footprint

--- src/footprints/trrs.js
+++ src/footprints/trrs.js
@@ -48,10 +48,13 @@ module.exports = {
       (fp_line (start 0.75 12.1) (end -5.35 12.1) (layer Dwgs.User) (width 0.15))
       (fp_line (start 0.75 0) (end -5.35 0) (layer Dwgs.User) (width 0.15))
 
-      ${''/* stabilizers */}
-      (pad "" np_thru_hole circle (at -2.3 8.6) (size 1.5 1.5) (drill 1.5) (layers *.Cu *.Mask))
-      (pad "" np_thru_hole circle (at -2.3 1.6) (size 1.5 1.5) (drill 1.5) (layers *.Cu *.Mask))
       `
+    function stabilizers(def_pos) {
+      return `
+        (pad "" np_thru_hole circle (at ${def_pos} 8.6) (size 1.5 1.5) (drill 1.5) (layers *.Cu *.Mask))
+        (pad "" np_thru_hole circle (at ${def_pos} 1.6) (size 1.5 1.5) (drill 1.5) (layers *.Cu *.Mask))
+      `
+    }
     function pins(def_neg, def_pos) {
       return `
         (pad 1 thru_hole oval (at ${def_neg} 11.3 ${p.rot}) (size 1.6 2.2) (drill oval 0.9 1.5) (layers *.Cu *.Mask) ${p.net.A.str})
@@ -63,18 +66,22 @@ module.exports = {
     if(p.param.reverse & p.param.symmetric) {
       return `
         ${standard}
+        ${stabilizers('-2.3')}
         ${pins('0', '-4.6')}
         ${pins('-4.6', '0')})
       `
     } else if(p.param.reverse) {
         return `
           ${standard}
+          ${stabilizers('-2.3')}
+          ${stabilizers('2.3')}
           ${pins('-4.6', '0')}
           ${pins('4.6', '0')})
         `
       } else {
         return `
           ${standard}
+          ${stabilizers('-2.3')}
           ${pins('-4.6', '0')})
         `
       }
