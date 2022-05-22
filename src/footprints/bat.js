3bf702797f6ac1863881ba1ef97f77a2a3f61eec
Ben Vallack <ben@cgdesign.co.nz>
2022-05-22 05:44:31 +0100

    fixes

--- src/footprints/bat.js
+++ src/footprints/bat.js
@@ -10,8 +10,8 @@ module.exports = {
     
     (module lib:bat (layer F.Cu) (tstamp 5BF2CC94)
         ${p.at /* parametric position */}
-	  (pad 1 thru_hole circle (at 0 -1.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.pos.str}) 
-	  (pad 2 thru_hole circle (at 0 1.62 0) (size 1.7526 1.7526) (drill 1.0922) (layers *.Cu *.SilkS *.Mask) ${p.net.neg.str})
+	  (pad 1 thru_hole circle (at 0 -0.75 0) (size 1 1) (drill 0.7) (layers *.Cu *.SilkS *.Mask) ${p.net.pos.str}) 
+	  (pad 2 thru_hole circle (at 0 0.75 0) (size 1 1) (drill 0.7) (layers *.Cu *.SilkS *.Mask) ${p.net.neg.str})
      )
 
     `
