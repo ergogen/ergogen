887220abe3069fe0deb2c4f9a951296cacfdcdb9
Bán Dénes <mr@zealot.hu>
2024-04-14 18:07:30 +0200

    Prevent circular dependencies in $extends

--- test/fixtures/bundle/templates/custom_template.js
+++ test/fixtures/bundle/templates/custom_template.js
@@ -4,6 +4,6 @@ const version = require('package.json').version
 module.exports = {
     convert_outline: () => {},
     body: params => {
-        return `Custom template override. The secret is ${params.custom.secret}. MakerJS is ${m.version}. Ergogen is ${version}.`
+        return `Custom template override. The secret is ${params.custom.secret}. MakerJS is ${m.version.length ? 'loaded' : ''}. Ergogen is ${version.length ? 'loaded' : ''}.`
     }
 }
