c44d5326a0d2ed038ad80fdd54ba6e5e9243957e
Marco Massarelli <mmassarelli@google.com>
2025-10-14 16:44:27 -0400

    Fix rollup.config.mjs (use Node 20+ import syntax)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,4 @@
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json' with { type: 'json' }
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 
