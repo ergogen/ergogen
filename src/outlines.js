67740334c42379c82eea914a17ddca88e7826e21
Marco Massarelli <mmassarelli@google.com>
2025-11-08 21:10:54 -0500

    Add clockwise and counter-clockwise options to hull configuration and update related DXF files

--- src/outlines.js
+++ src/outlines.js
@@ -117,10 +117,11 @@ const polygon = (config, name, points, outlines, units) => {
 const hull = (config, name, points, outlines, units) => {
 
   // prepare params
-  a.unexpected(config, `${name}`, ['concavity', 'extend', 'points'])
+  a.unexpected(config, `${name}`, ['concavity', 'extend', 'points', 'clockwise'])
   const concavity = a.sane(config.concavity || 50, `${name}.concavity`, 'number')(units)
   // Extend should default to `true` if not defined
   const extend = a.sane(config.extend === undefined || config.extend, `${name}.extend`, 'boolean')(units)
+  const clockwise = a.sane(config.clockwise === undefined || config.clockwise, `${name}.clockwise`, 'boolean')(units)
   const hull_points = a.sane(config.points, `${name}.points`, 'array')()
 
   // return shape function and its units
@@ -178,7 +179,7 @@ const hull = (config, name, points, outlines, units) => {
           parsed_points.push(last_anchor.p)
         }
     }
-    const poly_points = hulljs(parsed_points, concavity)
+    const poly_points = hulljs(parsed_points, concavity, undefined, clockwise)
     let poly = u.poly(poly_points)
     const bbox = u.bbox(poly_points)
     return [poly, bbox]
